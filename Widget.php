dab297012d4bc6821ebcfa04d6d14de3d0de2ff2
Vladislav Muschinskikh <i@unixoid.su>
2016-05-19 21:50:16 +0500

    upd

--- Widget.php
+++ Widget.php
@@ -1,6 +1,8 @@
 <?php namespace muvo\yii\slider;
 
 use yii\bootstrap\Html;
+use yii\helpers\Inflector;
+use yii\helpers\Json;
 use yii\web\View;
 
 class Widget extends \yii\bootstrap\Widget
@@ -19,11 +21,22 @@ class Widget extends \yii\bootstrap\Widget
     }
 
     public function run(){
+        /*
         $data = [ 'provide' => 'slider' ];
+
+        if(!empty($this->value))
+            $data['slider-value'] = $this->value;
+
         foreach($this->sliderOptions as $k=>$v)
             $data[sprintf('slider-%s',$k)] = $v;
 
         $this->options['data'] = $data;
+        */
+
+        $this->view->registerJs(sprintf('<!-- var slider%1$s = window.%1$s = new Slider(\'%2$s\',%3$s) -->',
+            Inflector::id2camel($this->id),
+            $this->id,
+            Json::encode($data)),View::POS_END);
 
         return Html::input($this->type,$this->name,$this->value,$this->options);
     }
