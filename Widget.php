bdc7a0dfe261073bbbdee93f1fad522214f8befe
Vladislav Muschinskikh <i@unixoid.su>
2016-05-19 21:53:33 +0500

    upd

--- Widget.php
+++ Widget.php
@@ -36,7 +36,7 @@ class Widget extends \yii\bootstrap\Widget
         $this->view->registerJs(sprintf('<!-- var slider%1$s = window.%1$s = new Slider(\'%2$s\',%3$s) -->',
             Inflector::id2camel($this->id),
             $this->id,
-            Json::encode($data)),View::POS_END);
+            Json::encode($this->sliderOptions)),View::POS_END);
 
         return Html::input($this->type,$this->name,$this->value,$this->options);
     }
